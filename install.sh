5c24fe084809fa1f58d24e8f624a2c6464b0c995
pingme998 <77038591+pingme998@users.noreply.github.com>
2022-01-30 13:27:44 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -30,5 +30,6 @@ curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|tee /etc/apt/sources.list.d/brave-browser-release.list
 
 apt update
+curl https://rclone.org/install.sh |bash
 apt-get autoclean -y 
 apt-get autoremove
