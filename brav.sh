af08f90daca81f16a0f95f7c0d234ab87e9547dd
pingme998 <77038591+pingme998@users.noreply.github.com>
2022-01-30 15:15:20 +0530

    Update brav.sh

--- brav.sh
+++ brav.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-brave-browser --no-sandbox
+brave-browser-stable --no-sandbox --disable-dev-shm-usage --disable-gpu --no-default-browser-check --disable-client-side-phishing-detection --deterministic-mode
+
