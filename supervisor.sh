7bb2efab29cf0c852313ba2eef47a043cb377d28
pingme998 <77038591+pingme998@users.noreply.github.com>
2022-01-30 13:25:47 +0530

    Update supervisor.sh

--- supervisor.sh
+++ supervisor.sh
@@ -2,5 +2,8 @@
 cd /
 git clone https://github.com/pingme998/brloop-test-2
 mv brloop-test-2 gitrepo
+chmod +x /gitrepo/permission.sh
+chmod +x /gitrepo/brav.sh
+bash /gitrepo/permission.sh
 set -ex
 exec supervisord -c /system/supervisord.conf
